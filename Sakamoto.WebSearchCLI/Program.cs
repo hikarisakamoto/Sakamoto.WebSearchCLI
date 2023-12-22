d4513f0721b5d733f9780886af9cb8809374ed0b
Sakamoto <hikarisakamoto@gmail.com>
2023-12-22 18:21:26 +0000

    Initial implementation using powershell.exe

--- Sakamoto.WebSearchCLI/Program.cs
+++ Sakamoto.WebSearchCLI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var url = "https://www.google.com"; // The URL to open
+
+var psi = new ProcessStartInfo
+{
+    FileName = "powershell.exe",
+    Arguments = $"-Command \"Start-Process msedge '{url}'\"",
+    UseShellExecute = false,
+    CreateNoWindow = true // Prevents opening an additional PowerShell window
+};
+
+try
+{
+    Process.Start(psi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
